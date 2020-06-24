37d7b91b31d6379c376078f3bce74e3e2a79fb3c
daaavus <daaavus@gmail.com>
2020-06-24 09:03:30 +0300

    fixed

--- scripts/script.js
+++ scripts/script.js
@@ -3,27 +3,29 @@ let editBtn = document.querySelector('.profile__edit-button');
 let closeBtn = document.querySelector('.popup__close-button');
 let saveBtn = document.querySelector('.popup__save-button');
 
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__rank');
+let nameInput = document.getElementById('nameInput');
+let jobInput = document.getElementById('jobInput');
 
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 
-editBtn.addEventListener("click", function(event){
+function openPopup(event){
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
   event.preventDefault();
-  popup.classList.remove("popup__hidden");
-});
+  popup.classList.add("popup__opened");
+}
+
+editBtn.addEventListener("click", openPopup)
 
 closeBtn.addEventListener("click", function(event){
   event.preventDefault();
-  popup.classList.add("popup__hidden");
+  popup.classList.remove("popup__opened");
 });
 
 saveBtn.addEventListener("click", function(event){
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popup.classList.add("popup__hidden");
+  popup.classList.remove("popup__opened");
 });
