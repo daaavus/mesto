79ca014cc7918fc357a5ec040334ed033c211f25
daaavus <daaavus@gmail.com>
2020-06-27 02:34:03 +0300

    hotfix

--- scripts/script.js
+++ scripts/script.js
@@ -13,14 +13,14 @@ function openPopup(event){
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
   event.preventDefault();
-  popup.classList.add("popup__opened");
+  popup.classList.add("popup_opened");
 }
 
 editBtn.addEventListener("click", openPopup)
 
 function closePopup(event){
   event.preventDefault();
-  popup.classList.remove("popup__opened");
+  popup.classList.remove("popup_opened");
 }
 
 closeBtn.addEventListener("click", closePopup);
@@ -29,7 +29,7 @@ function savePopup(event){
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popup.classList.remove("popup__opened");
+  popup.classList.remove("popup_opened");
 }
 
 saveBtn.addEventListener("click", savePopup);
