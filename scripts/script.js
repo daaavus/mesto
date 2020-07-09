27d244688457227358a3ac03fd63d505e958eb04
daaavus <daaavus@gmail.com>
2020-07-09 18:02:14 +0300

    added image-popup

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,11 @@ const closeElementsBtn = elementsPopup.querySelector('.popup__close-elements-but
 const mestoInput = elementsPopup.querySelector('.popup__mesto');
 const linkInput = elementsPopup.querySelector('.popup__link');
 const elementsPopupSave = elementsPopup.querySelector('.popup__container');
+const imagePopup = document.querySelector('.image-popup');
+const imagePopupCloseBtn = imagePopup.querySelector('.image-popup__close-button');
+const imagePopupImage = imagePopup.querySelector('.image-popup__image');
+const imagePopupCaption = imagePopup.querySelector('.image-popup__caption');
+const imagePopupContainer = imagePopup.querySelector('.image-popup__container');
 
 let nameInput = document.getElementById('nameInput');
 let jobInput = document.getElementById('jobInput');
@@ -87,6 +92,10 @@ function deleteCard (event) {
   element.remove();
 };
 
+function toggleImagePopup(){
+  imagePopup.classList.toggle('popup_opened');
+};
+
 function addCard(item){
   const cardTemplate = document.querySelector('#card-template').content;
   const card = cardTemplate.cloneNode(true);
@@ -94,6 +103,13 @@ function addCard(item){
   card.querySelector('.element__pic').src = item.link;
   card.querySelector('.element__heart').addEventListener('click', likeCard);
   card.querySelector('.element__musorka').addEventListener('click', deleteCard);
+  card.querySelector('.element__pic').addEventListener('click', function (event) {
+    event.target.closest('.popup__img');
+    imagePopupImage.src = item.link;
+    imagePopupImage.alt = item.name;
+    imagePopupCaption.textContent = item.name;
+    toggleImagePopup();
+  });
 
   cardsContainer.prepend(card);
 }
@@ -111,3 +127,6 @@ elementsPopupSave.addEventListener('submit', event => {
   addCard(cardItem);
   toggleElementsPopup(event);
 });
+
+imagePopupCloseBtn.addEventListener('click', toggleImagePopup)
+
